8baa16a286f7a948ea4f2a60759877146aa8d4fb
SperandioR <rodrigopaulino13@gmail.com>
2023-01-19 01:56:03 -0300

    Alterando o Name para Frmlogin

--- SistemaHotel/Login.Designer.cs
+++ SistemaHotel/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaHotel
 {
-    partial class Login
+    partial class FrmLogin
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,29 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.pnlLogin = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel1
+            // pnlLogin
             // 
-            this.panel1.Location = new System.Drawing.Point(308, 147);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 154);
-            this.panel1.TabIndex = 0;
+            this.pnlLogin.Location = new System.Drawing.Point(444, 144);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(350, 350);
+            this.pnlLogin.TabIndex = 0;
             // 
-            // Login
+            // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1168, 628);
+            this.Controls.Add(this.pnlLogin);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.Name = "Login";
+            this.Name = "FrmLogin";
             this.Text = "Login";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
@@ -59,7 +59,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlLogin;
     }
 }
 
--- SistemaHotel/Login.cs
+++ SistemaHotel/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SistemaHotel
 {
-    public partial class Login : Form
+    public partial class FrmLogin : Form
     {
-        public Login()
+        public FrmLogin()
         {
             InitializeComponent();
         }
