fe395f234969a84228cc8993dc4e8a3ddfe528a7
SperandioR <rodrigopaulino13@gmail.com>
2023-02-13 13:58:00 -0300

    Na tela FrmFuncionario acrescentamos Label e TxtBox; e alteramos suas propriedades.

---
+++ SistemaHotel/Cadastros/Funcionarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaHotel.Cadastros
+{
+    public partial class Funcionarios : Form
+    {
+        public Funcionarios()
+        {
+            InitializeComponent();
+        }
+    }
+}
